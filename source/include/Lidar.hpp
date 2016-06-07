ed8770af9d0399515607343b4249c848c52a087b
jessy100 <jessyvisch@gmail.com>
2016-06-07 12:20:41 +0200

    fixed out of bounds for loop reading

--- source/include/Lidar.hpp
+++ source/include/Lidar.hpp
@@ -3,18 +3,18 @@ class Lidar : public LocatedDistanceSensor{
 
 public:
 	Lidar (double factor, CoordinateBearing coordinate_bearing);
-	
+
 	SensorResult get_data() override {
 		std::vector<r2d2::Angle,r2d2::Length > Angles = comminucatie_stub.get_angles();
 		PolarView v();
-		for (int i = 0; i >0; i++){
+		for (int i = 0; i < size(Angles); i++){
 			v.add_distancereading(Angles[i], r2d2::Length len,
                              DistanceReading::ResultType type);
 		}
 	}
-	
-	
-	
+
+
+
 private:
 	Comminucatie comminucatie_stub = new Comminucatie();
 }
